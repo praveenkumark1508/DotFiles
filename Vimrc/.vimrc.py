a87b9faa40f54b6d0a8306ad0f48f5cb2493343b
Praveen Kumar K <praveenkumark1508@gmail.com>
2018-09-23 19:00:16 +0530

    fixing bug in vimrc.py

--- Vimrc/.vimrc.py
+++ Vimrc/.vimrc.py
@@ -1,18 +1,18 @@
 # vim: set sw=4 ts=4 sts=4 et tw=78 foldmarker=@{,@} foldlevel=0 foldmethod=marker:
 
+import snake
+from snake import command, expand
 import os
 import sys
 import re
-import snake
-from snake import command, expand
 
 # Useful Global variables
-HOME = os.path.join(os.getenv("HOME")
+HOME = os.getenv("HOME")
 
 # Build a project
 @snake.key_map("<leader>bs")
 def build_project():
-    user_input_file = HOME, "vim_userinputs.conf")
+    user_input_file = os.path.join(HOME, "vim_userinputs.conf")
 
     with open(user_input_file, 'r') as fileobj:
         for line in fileobj:
